9f2635145e63bbe0719b16f95358d2903ea6a8ec
YapTrap <280863229+YapTrapp@users.noreply.github.com>
2026-07-27 20:11:00 -0500

    Add button to take user input for grid size

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const container = document.getElementById("grid-container");
+const sizeBtn = document.getElementById("size-btn");
 
-createGrid();
+sizeBtn.addEventListener("click", () => {
+    let size = Number(prompt("Enter grid size (e.g. 16 for 16x16):"));
+    if(!size || size<1 || size>100){
+        alert("Please enter a number between 1 and 100.");
+        return;
+    }
+    createGrid(size);
+});
+
 
 function createGrid(size){
+    container.innerHTML = "";
     const boxSize = 100 / size;
 
     for(let i=1; i<=size; i++){
