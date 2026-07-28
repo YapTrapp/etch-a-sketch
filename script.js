ebc1dcd6b33e5b74576b5a197b15b702aea0e8a1
YapTrap <280863229+YapTrapp@users.noreply.github.com>
2026-07-27 19:01:37 -0500

    Add size parameter to createGrid

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ const container = document.getElementById("grid-container");
 
 createGrid();
 
-function createGrid(){
-    for(let i=1; i<=8; i++){
-        for(let j=1; j<=8; j++){
+function createGrid(size){
+    const boxSize = 100 / size;
+
+    for(let i=1; i<=size; i++){
+        for(let j=1; j<=size; j++){
             const gridBox = document.createElement("div");
+            
             gridBox.classList.add("grid-box");
+            gridBox.style.width = `${boxSize}%`;
+            gridBox.style.height = `${boxSize}%`;
             container.appendChild(gridBox);
         }
     }
